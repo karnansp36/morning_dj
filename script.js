a4f71c505fb8ffc6c68353d2b0517c89f383d362
karnansp36 <reviewmaster36@gmail.com>
2025-06-16 11:25:44 +0530

    updated the script and style

--- script.js
+++
@@ -1,8 +0,0 @@
-let arr = [45,56,78,6,56,34]
-   
-let arr2 = ['benz','audi','jaguar','bmw','toyata','Astro martin','Zen'];
-
-
-console.log(arr2.sort())
-console.log(arr.sort((a,b)=>b-a))
-console.log(arr.reverse())
